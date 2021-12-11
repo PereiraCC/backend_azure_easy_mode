e0cbeb6720e187211b27c2e56d7ad32bc239666f
Carlos Pereira <carloscoto0103@gmail.com>
2021-12-11 17:18:48 -0600

    config.ts: added storage configuration

--- db/config.ts
+++ db/config.ts
@@ -1,10 +1,17 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/storage";
+// import firebaseAdmin from "firebase-admin";
+// import serviceAccount from './flutter-push-6b9f4-firebase-adminsdk-ok61y-e40e381168.json';
 
 // Import and configure dotenv
 import dotenv from 'dotenv';
 dotenv.config();
+
+// const admin = firebaseAdmin.initializeApp({
+//     credential: firebaseAdmin.credential.cert(JSON.stringify(serviceAccount))
+// })
 
 const firebaseConfig = {
     apiKey            : process.env.APIKEYDB,
@@ -18,7 +25,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 console.log('Firabase is ready');
 
-export default firebase.firestore();
+// export const storageRef = admin.storage().bucket(`gs://flutter-push-6b9f4.appspot.com`);
+export const storageRef = firebase.storage().ref();
+export const firestore = firebase.firestore();
--- dist/db/config.js
+++ dist/db/config.js
@@ -3,12 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.firestore = exports.storageRef = void 0;
 // Import the functions you need from the SDKs you need
 const app_1 = __importDefault(require("firebase/app"));
 require("firebase/firestore");
+require("firebase/storage");
+// import firebaseAdmin from "firebase-admin";
+// import serviceAccount from './flutter-push-6b9f4-firebase-adminsdk-ok61y-e40e381168.json';
 // Import and configure dotenv
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// const admin = firebaseAdmin.initializeApp({
+//     credential: firebaseAdmin.credential.cert(JSON.stringify(serviceAccount))
+// })
 const firebaseConfig = {
     apiKey: process.env.APIKEYDB,
     authDomain: process.env.AUTHDOMAINDB,
@@ -21,5 +28,7 @@ const firebaseConfig = {
 // Initialize Firebase
 app_1.default.initializeApp(firebaseConfig);
 console.log('Firabase is ready');
-exports.default = app_1.default.firestore();
+// export const storageRef = admin.storage().bucket(`gs://flutter-push-6b9f4.appspot.com`);
+exports.storageRef = app_1.default.storage().ref();
+exports.firestore = app_1.default.firestore();
 //# sourceMappingURL=config.js.map
